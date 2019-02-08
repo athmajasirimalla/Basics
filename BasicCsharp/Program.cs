ac1445df8debfd160f06976fcffc4918804af681
Chandra <Chandra@DESKTOP-ONCETEG>
2019-02-08 15:36:32 +0530

    Class Program

--- BasicCsharp/Program.cs
+++ BasicCsharp/Program.cs
@@ -6,6 +6,40 @@ using System.Threading.Tasks;
 
 namespace BasicCsharp
 {
+
+    class A
+    {
+        int _a;
+        int _b;
+
+
+        public A(int a, int b)
+        {
+            _a = a;
+            _b = b;
+
+        }
+        public void Sum()
+        {
+            Console.WriteLine("Sum={0}", _a+_b);
+        }
+    }
+        class program
+        {
+            public static void Main()
+            {
+                A A1 = new A(10, 20);
+                A1.Sum();
+
+            }
+
+
+        }
+    
+}
+    
+//Interface
+
     class Program
     {
         static void Main()
