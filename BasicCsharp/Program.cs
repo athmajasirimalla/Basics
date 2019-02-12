8ac2e3a33d7b8ffe0aa2c5b04190bb70e4b6f3df
Chandra <Chandra@DESKTOP-ONCETEG>
2019-02-12 15:01:51 +0530

    encapsulation

--- BasicCsharp/Program.cs
+++ BasicCsharp/Program.cs
@@ -4,68 +4,95 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace BasicCsharp
+namespace Encapsulation
+
 {
 
-   /* class A
+    /* class A
+     {
+         int _a;
+         int _b;
+
+
+         public A(int a, int b)
+         {
+             _a = a;
+             _b = b;
+
+         }
+         public void Sum()
+         {
+             Console.WriteLine("Sum={0}", _a+_b);
+         }
+     }
+         class program
+         {
+             public static void Main()
+             {
+                 A A1 = new A(10, 20);
+                 A1.Sum();
+
+             }
+
+
+         }
+
+ }*/
+
+    //Interface
+
+    class Bank
     {
-        int _a;
-        int _b;
-
-
-        public A(int a, int b)
+        public string BankName { get; set; }
+        public string Branchname { get; set; }
+        public int BranchCode { get; set; }
+        public string BankDetails()
         {
-            _a = a;
-            _b = b;
-
-        }
-        public void Sum()
-        {
-            Console.WriteLine("Sum={0}", _a+_b);
-        }
-    }
-        class program
-        {
-            public static void Main()
-            {
-                A A1 = new A(10, 20);
-                A1.Sum();
-
-            }
-
-
-        }
-    
-}*/
-    
-//Interface
-
-    class Program
-    {
-        static void Main()
-        {
+            BankName = "Indusind Bank";
+            Branchname = "Brookfield";
+            BranchCode = 1234;
+            Console.WriteLine(BankName + "-" + BranchCode + "-" + Branchname);
+            return "";
             
-            Interface1 A1 = new A();
-            Interface2 B1 = new B();
-            A1.ToffeeA();
-            B1.ToffeeB();
-           
         }
-        class A : Interface1
+        class Customer
         {
-            void Interface1.ToffeeA()
+            public string CustomerName { get; set; }
+            public int CustomerId { get; set; }
+            private UInt32 CustomerAcc { get; set; }
+            public int CustomerDetails()
             {
-                Console.WriteLine("HELLO WORLD!");
-            }
-        }
-        class B : Interface2
-        {
-            void Interface2.ToffeeB()
-            {
-                Console.WriteLine("WELCOME!");
+                CustomerName = "scott";
+                CustomerId = 234;
+                CustomerAcc = 1234567;
+                Console.WriteLine(CustomerName + "-" + CustomerId + "-" + CustomerAcc);
+                return 1;
             }
         }
 
-
+        public static void Main()
+        {
+            Bank B1 = new Bank();
+            B1.BankDetails();
+            Customer C1 = new Customer();
+            C1.CustomerDetails();
+            }
     }
 }
+
+        /*  class Customer
+          {
+              public string CustomerName { get; set; }
+              public int CustomerId { get; set; }
+              public string CustomerDetails()
+              {
+                  CustomerName = "Scott";
+                  CustomerId = 234;
+                  return CustomerName;
+              }*/
+
+      //
+    
+
+        
+        
